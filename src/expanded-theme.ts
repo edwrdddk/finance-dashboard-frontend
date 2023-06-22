19ba47a0c4db6e7922332e821c776b06964da660
Edward Didyk <eduard.didyk01@gmail.com>
2023-06-22 17:23:48 +0300

    expanded-theme

---
+++ src/expanded-theme.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line 
+import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+
+declare module "@mui/material/styles/createPalette" {
+  interface PaletteColor {
+    [key: number]: string;
+  }
+
+  interface Palette {
+    tertiary: PaletteColor;
+  }
+}
+
+// In case I forget, what is happening here is extending a default interface. 
