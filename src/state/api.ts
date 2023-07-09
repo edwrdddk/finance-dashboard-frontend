8a41a24247d0bb728ec5085161b86c8307cb8ffd
Edward Didyk <eduard.didyk01@gmail.com>
2023-07-10 02:43:21 +0300

    getTransactions api + GetTransactionsResponse interface

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,6 +1,10 @@
 /* TO GRAB DATA FROM BACKEND */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpisResponse, GetProductsResponse } from "./types";
+import {
+  GetKpisResponse,
+  GetProductsResponse,
+  GetTransactionsResponse,
+} from "./types";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
@@ -14,6 +18,10 @@ export const api = createApi({
     getProducts: build.query<Array<GetProductsResponse>, void>({
       query: () => "product/products/",
       providesTags: ["Products"],
+    }),
+    getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+      query: () => "transaction/transactions/",
+      providesTags: ["Transactions"],
     }),
   }),
 });
