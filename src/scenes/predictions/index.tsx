d1a8f476ca15c74e3bedd5ceb316bdd038c51180
Edward Didyk <eduard.didyk01@gmail.com>
2023-07-14 00:26:03 +0300

    Predictions in progress 1.0

---
+++ src/scenes/predictions/index.tsx
@@ -0,0 +1,20 @@
+import DashboardBox from "@/components/DashboardBox";
+import { useGetKpisQuery } from '@/state/api';
+import { Box, Button, Typography, useTheme } from "@mui/material";
+import React, { useState } from 'react'
+
+type Props = {}
+
+const Predictions = (props: Props) => {
+  const { palette } = useTheme();
+  const [isPredictions, setIsPredictions] = useState(false);
+  const { data: kpiData } = useGetKpisQuery();
+
+  return (
+    <DashboardBox>
+      
+    </DashboardBox>
+  )
+}
+
+export default Predictions
